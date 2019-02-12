ac6e199a303d449e32f688ce73426f048fe0388a
Joemar Taganna <joemar.ct@gmail.com>
2019-02-12 22:50:43 +0800

    Added simple backend, working contact form

--- webapp/src/components/ContactFormModal.vue
+++ webapp/src/components/ContactFormModal.vue
@@ -3,24 +3,33 @@
     ref="modal"
     title="Contact Form"
     @hidden="cancelContactForm"
+    ok-title="Submit"
+    @ok="submitContactForm"
     :hide-footer="!showSubmitButton"
   >
-    <form @enter.prevent="submitContactForm">
+    <p v-if="emailSendingSuccess === true" style="font-size: 24px; color: green;">
+      Your message has been sent successfully! We will get in touch as soon as possible.
+    </p>
+    <form v-else @enter.prevent="submitContactForm">
       <b-form-group label="Name:">
         <b-input
           name="name"
+          v-model="email.name"
           required
         ></b-input>
       </b-form-group>
       <b-form-group label="Email:">
         <b-input
           name="email"
+          type="email"
+          v-model="email.email"
           required
         ></b-input>
       </b-form-group>
       <b-form-group label="Message:">
         <b-textarea rows="4"
           name="message"
+          v-model="email.message"
           required
         ></b-textarea>
       </b-form-group>
@@ -35,12 +44,15 @@
 
 <script>
 import VueRecaptcha from 'vue-recaptcha'
+import axios from 'axios'
 
 export default {
   data () {
     return {
       sitekey: '6Ld27pAUAAAAAGq3D0WIqaZPCjfCi3YUNtx-sVSA',
-      showSubmitButton: false
+      showSubmitButton: false,
+      email: {},
+      emailSendingSuccess: null
     }
   },
   components: { VueRecaptcha },
@@ -49,7 +61,14 @@ export default {
       this.$refs.modal.show()
     },
     cancelContactForm () {},
-    submitContactForm () {},
+    submitContactForm (event) {
+      event.preventDefault()
+      let vm = this
+      axios.post('/mailer', this.email).then(function (resp) {
+        vm.emailSendingSuccess = resp.data.success
+        vm.showSubmitButton = false
+      })
+    },
     onVerify () {
       this.showSubmitButton = true
     },
