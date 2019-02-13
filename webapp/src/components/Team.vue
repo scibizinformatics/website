311aa9a28ddc3b1b0a8b57fdfb11334b8a4e900a
Joemar Taganna <joemar.ct@gmail.com>
2019-02-13 12:52:40 +0800

    Team members pictures styling and transition

--- webapp/src/components/Team.vue
+++ webapp/src/components/Team.vue
@@ -1,43 +1,43 @@
 <template>
   <div id="team">
     <b-row>
-      <b-col class="member">
-        <img src="../assets/team/joemar.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/joemar.png">
         <div class="details">
           <span class="name">Joemar Taganna</span><br>
           <span class="role">Founder & Lead Programmer</span>
         </div>
       </b-col>
-      <b-col class="member">
-        <img src="../assets/team/reamon.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/reamon.png">
         <div class="details">
           <span class="name">Reamon Sumapig</span><br>
           <span class="role">Senior Programmer</span>
         </div>
       </b-col>
-      <b-col class="member">
-        <img src="../assets/team/jessie.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/jessie.png">
         <div class="details">
           <span class="name">Jesus Taganna, Jr.</span><br>
           <span class="role">Senior Programmer</span>
         </div>
       </b-col>
-      <b-col class="member">
-        <img src="../assets/team/person.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/person.png">
         <div class="details">
           <span class="name">Syryll Anne Pajares</span><br>
           <span class="role">Multimedia Specialist</span>
         </div>
       </b-col>
-      <b-col class="member">
-        <img src="../assets/team/person.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/person.png">
         <div class="details">
           <span class="name">Lynie Pensona</span><br>
           <span class="role">Secretary/Bookkeeper</span>
         </div>
       </b-col>
-      <b-col class="member">
-        <img src="../assets/team/person.png" class="circular-img">
+      <b-col sm="12" md="2" class="member">
+        <img src="../assets/team/person.png">
         <div class="details">
           <span class="name">Geraldine Taganna</span><br>
           <span class="role">Admin Manager</span>
@@ -54,20 +54,28 @@ export default {
 </script>
 
 <style scoped>
-.circular-img {
-  border-radius: 50%;
-  border: 1px solid lightgrey;
-}
 .member {
   text-align: center;
 }
 .member img {
+  border-radius: 50%;
+  border: 1px solid lightgrey;
   width: 140px;
   -webkit-filter: grayscale(90%);
   filter: grayscale(90%);
+  transition: filter 1.5s;
+  transition: background 2s;
+  transition: border 1s;
+}
+@media screen and (max-width: 600px) {
+  .member {
+    margin-bottom: 15px;
+  }
 }
 .member img:hover {
+  background: #5fbcd3;
   -webkit-filter: grayscale(0%);
+  border: 4px groove #0044aa;
   filter: grayscale(0%);
 }
 .details {
